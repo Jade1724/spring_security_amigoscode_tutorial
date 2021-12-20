19a91a70b0f4fbb1e5af505c0866df31bd4e8ce3
Haruka Ichinose <hic21@uclive.ac.nz>
2021-12-20 17:18:38 +1300

    First commit, student controller and spring web and security dependencies installed

---
+++ src/main/java/com/example/amigoscode_tutorial/AmigoscodeTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.amigoscode_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodeTutorialApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AmigoscodeTutorialApplication.class, args);
+  }
+
+}
