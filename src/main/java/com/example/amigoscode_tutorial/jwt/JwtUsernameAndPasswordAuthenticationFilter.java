3b0733d6d248e8895e116e969bad40d379b42305
Haruka Ichinose <hic21@uclive.ac.nz>
2021-12-27 18:30:06 +1300

    Implemented a method to generate a JWT and attach to response header.

--- src/main/java/com/example/amigoscode_tutorial/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/example/amigoscode_tutorial/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,7 +1,13 @@
 package com.example.amigoscode_tutorial.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -39,5 +45,24 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
     }
 
   }
+
+  @Override
+  protected void successfulAuthentication(HttpServletRequest request,
+                                          HttpServletResponse response,
+                                          FilterChain chain,
+                                          Authentication authResult) throws IOException, ServletException {
+    String key = "securesecuresecuresecuresecuresecuresecuresecuresecure";
+
+    String token = Jwts.builder()
+        .setSubject(authResult.getName())
+        .claim("authorities", authResult.getAuthorities())
+        .setIssuedAt(new Date())
+        .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+        .signWith(Keys.hmacShaKeyFor(key.getBytes()))
+        .compact();
+
+    response.addHeader("Authorization", "Bearer " + token);
+  }
+
 
 }
