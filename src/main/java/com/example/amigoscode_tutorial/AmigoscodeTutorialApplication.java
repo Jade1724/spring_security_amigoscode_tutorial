ad64cac88bae180fee20708a56e188d98635eedb
Haruka Ichinose <hic21@uclive.ac.nz>
2021-12-28 17:39:53 +1300

    Minor fix. Replaced hardcoded token lifetime with application properties variable, removed unused imports.

--- src/main/java/com/example/amigoscode_tutorial/AmigoscodeTutorialApplication.java
+++ src/main/java/com/example/amigoscode_tutorial/AmigoscodeTutorialApplication.java
@@ -2,8 +2,6 @@ package com.example.amigoscode_tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class AmigoscodeTutorialApplication {
